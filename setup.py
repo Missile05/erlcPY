96361a61b77c68af9aa6dcdfe4f5f9b6f806821e
Missile <j.l.conquest07@gmail.com>
2024-02-25 08:44:43 +0000

    V1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='erlcpy',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=['requests'],
     author='Missile / Arimuon',
