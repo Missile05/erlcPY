a500dabe8287b638c21eee1c8ac28ad98d3dfa4e
Missile <j.l.conquest07@gmail.com>
2024-02-25 08:21:38 +0000

    V1.0.1

--- erlcpy/__init__.py
+++ erlcpy/__init__.py
@@ -5,5 +5,6 @@ from .main import get_queue
 from .main import get_kill_logs
 from .main import get_command_logs
 from .main import send_command
-from .main importget_mod_calls
-from .main importget_bans
+from .main import get_mod_calls
+from .main import get_bans
+from .main import send_command
